ea8b895baf0fc0c408d308ea6a1697cbe924603d
marvaummer <marvaummer.nu@gmail.com>
2023-10-07 09:07:00 +0530

    Initial commit

---
+++ android/app/src/main/java/com/marva/my_chat_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.marva.my_chat_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/shared/constants.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+
+class Constants{
+  static String appId="1:268695774378:web:96b3741e8bc63bca161cd";
+  static String apiKey= "AIzaSyB4B6XTXRE6zHJsCAYkBlcEQAuYyeS1X1o";
+  static String messagingSenderId= "268695774378";
+  static String projectId="mychatapp-9e962";
+  final primaryColor=Color(0xFFee7b64);
+}
